02441c4e932c67ca67b1a3588443aa37153e9ac3
SL-Lee <45960387+SL-Lee@users.noreply.github.com>
2021-08-23 13:49:16 +0800

    Re-export `gcd`, `lcm`, and `is_prime`

--- src/lib.rs
+++ src/lib.rs
@@ -1,5 +1,8 @@
 pub mod binary_search;
 pub mod factorial;
 pub mod fibonacci;
-pub mod gcd_lcm;
-pub mod is_prime;
+mod gcd_lcm;
+mod is_prime;
+
+pub use gcd_lcm::{gcd, lcm};
+pub use is_prime::is_prime;
